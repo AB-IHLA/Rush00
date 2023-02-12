192f906d6649ff33779abfcbcd524612d774e824
ibrahimabdramane62@gmail.com <ibrahimabdramane62@gmail.com>
2023-02-13 00:03:17 +0100

    Rush00

---
+++ numberToString.js
@@ -0,0 +1,12 @@
+function numberToString (){
+  Entree :  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+     
+     sortie : ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"];
+       
+       module.exports = numberToString
+  
+      }console.log (numberToString );
+      
+      
